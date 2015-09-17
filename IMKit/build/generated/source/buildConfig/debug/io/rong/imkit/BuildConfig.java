26d3b1624887a8d11584f3877d92d7f199670b3f
Bob <ityangb@163.com>
2015-09-17 10:44:59 +0800

    sync 2.3.0

--- IMKit/build/generated/source/buildConfig/debug/io/rong/imkit/BuildConfig.java
+++ IMKit/build/generated/source/buildConfig/debug/io/rong/imkit/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "io.rong.imkit";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 2015060820;
-  public static final String VERSION_NAME = "2.1.1stable";
+  public static final int VERSION_CODE = 2015091422;
+  public static final String VERSION_NAME = "2.3.0 Stable";
 }
